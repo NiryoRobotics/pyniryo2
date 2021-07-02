7febc17ab50751605f2eac5d1541a62e85e7d965
Valentin Pitre <v.pitre@niryo.com>
2021-07-02 15:51:45 +0200

    add packages in robot class

--- pyniryo2/niryo_robot.py
+++ pyniryo2/niryo_robot.py
@@ -6,9 +6,12 @@ import roslibpy
 import time
 
 # Communication imports
-from .exceptions import *
-from pyniryo2.arm.arm import Arm
-from pyniryo2.trajectories.trajectories import Trajectories
+from .arm.arm import Arm
+from .io.io import IO
+from .saved_poses.saved_poses import SavedPoses
+from .tool.tool import Tool
+from .trajectories.trajectories import Trajectories
+from .vision.vision import Vision
 
 
 class NiryoRobot(object):
@@ -20,7 +23,11 @@ class NiryoRobot(object):
         self.connect(host, port)
 
         self.__arm = Arm(self.__client)
+        self.__io = IO(self.__client)
+        self.__saved_poses = SavedPoses(self.__client)
+        self.__tool = Tool(self.__client)
         self.__trajectories = Trajectories(self.__client)
+        self.__vision = Vision(self.__client)
 
     def __del__(self):
         if self.__client:
@@ -58,6 +65,22 @@ class NiryoRobot(object):
         return self.__arm
 
     @property
+    def io(self):
+        return self.__io
+
+    @property
+    def saved_poses(self):
+        return self.__saved_poses
+
+    @property
+    def tool(self):
+        return self.__tool
+
+    @property
     def trajectories(self):
         return self.__trajectories
+
+    @property
+    def vision(self):
+        return self.__vision
 
